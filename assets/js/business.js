81de727ee38e0991e0bcde5691c45ae8f0a16361
matheusbelkman <matheuspozza2021@hotmail.com>
2026-08-13 10:31:21 -0300

    feat: add official business data

---
+++ assets/js/business.js
@@ -0,0 +1,16 @@
+export const business = {
+    name: 'Espaço Delícia',
+    phone: '(14) 99798-0908',
+    whatsapp: '5514997980908',
+    address: 'Rua 15 de Novembro, 787. Centro, Timburi/SP.',
+    openingHours: [
+        { day: 'Segunda-feira', hours: 'Fechado' },
+        { day: 'Terça-feira', hours: 'Fechado' },
+        { day: 'Quarta-feira', hours: '14:00 às 20:00' },
+        { day: 'Quinta-feira', hours: '14:00 às 20:00' },
+        { day: 'Sexta-feira', hours: '14:00 às 21:00' },
+        { day: 'Sábado', hours: '14:00 às 21:00' },
+        { day: 'Domingo', hours: '14:00 às 20:00' },
+    ],
+    deliveryFee: 0,
+};
